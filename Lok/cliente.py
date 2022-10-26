63a979fbf9ea3e2dd4e4c5d4a9c7dd4bf66dc405
Ordep <58833700+SrStark666@users.noreply.github.com>
2022-10-26 03:16:48 -0300

    Add files via upload

--- Lok/cliente.py
+++ Lok/cliente.py
@@ -1,1 +1,59 @@
-wdwdw
+import socket
+from  datetime import *
+from getpass import getpass
+from colorama import init, Fore
+
+
+host = "127.0.0.1"
+port = 5062
+address = (host, port)
+init(convert=True)
+
+while True:
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.connect((host, port))
+    
+    data = datetime.today().strftime('%Y/%m/%d')
+    hora = datetime.today().strftime('%H:%M')
+
+    print(f"""{Fore.GREEN}[+]Bem vindo ao Servidor[+]{Fore.RESET}
+        [1]Login
+        [2]Registro
+        [3]Sair
+    """)
+
+    choice = int(input("==> "))
+
+    if choice == 1:
+        tcp.send(bytes("logar", encoding="utf-8"))
+
+        nome_login = input("Digite seu login: ")
+        senha_login = getpass("Digite sua senha: ")
+
+        tcp.send(bytes(nome_login, encoding="utf-8"))
+        tcp.send(bytes(senha_login, encoding="utf-8"))
+
+        package = tcp.recv(1024).decode("utf-8")
+        if package == "False":
+            print(f"{Fore.RED}Login inválido, faça um registro!!{Fore.RESET}")
+            continue
+
+        else:
+            tcp.send(bytes(f"Nome {nome_login} Senha {senha_login} Data {data} Hora {hora}", encoding="utf-8"))
+            print(package)
+
+    elif choice == 2:
+        tcp.send(bytes("registro", encoding="utf-8"))
+        login = str(input("Digite seu login: "))
+        password = getpass("Digite sua senha: ")
+
+        tcp.send(bytes(login, encoding="utf-8"))
+        tcp.send(bytes(password, encoding="utf-8"))
+
+    elif choice == 3:
+        tcp.close()
+        break
+
+
+
+   
--- Lok/servidor.py
+++ Lok/servidor.py
@@ -1,1 +1,46 @@
-dwdwadw
+from distutils.command.config import config
+import socket
+from database.configs import *
+import subprocess
+import os
+
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1"
+port = 5062
+
+address = (host, port)
+tcp.bind(address)
+tcp.listen(3)
+
+try:
+    clientes = []
+    while True:
+        session, client = tcp.accept()
+        clientes.append(session)
+        if client:
+            print("Acesso ao menu identificado!!")
+            verificar_choice = session.recv(1024).decode("utf-8")
+            if not verificar_choice:
+                continue
+            else:
+                if verificar_choice == "logar":
+                    verificar_login = session.recv(1024).decode("utf-8")
+                    verificar_senha = session.recv(1024).decode("utf-8")
+                    login(verificar_login, verificar_senha, session, client)
+                    subprocess.run("start chk.exe", shell=True)
+                        
+                elif verificar_choice == "registro":
+                    print("Registro acessado")
+                    create = []
+                    for i in range(2):
+                        login = session.recv(1024).decode("utf-8")
+                        create.append(login)
+                            
+                    register(create[0], create[1])
+                    print("Registro efetuado")
+                    continue
+                    
+            
+except KeyboardInterrupt:
+    print("Programa encerrado!!")
